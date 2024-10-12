ba22c34a4684f05818d518d5531de7df2022a0f9
jadkarr <129285275+jadkarr@users.noreply.github.com>
2024-10-12 14:05:29 +0300

    Simple RecipeService
    RecipeController and RecipeService

--- src/main/java/com/RecipeFinder/backend/services/RecipeService.java
+++ src/main/java/com/RecipeFinder/backend/services/RecipeService.java
@@ -1,27 +1,32 @@
 package com.RecipeFinder.backend.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 import com.RecipeFinder.backend.models.Recipe;
 import com.RecipeFinder.backend.repositories.RecipeRepository;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
 
 @Service
 public class RecipeService {
-
-    private final RecipeRepository recipeRepository;
-
-    public RecipeService(RecipeRepository recipeRepository) {
-        this.recipeRepository = recipeRepository;
-    }
-
-    /* Example */
-    public Recipe getRecipeById(Long id) {
+    private final String apiUrl = "https://api.spoonacular.com/recipes/complexSearch";
+    private final String apiKey = "4b2cdc4ff73546b89cc40882c81c8e9c";
+    
+    
+    public void fetchRecipes() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = apiUrl + "?apiKey=" + apiKey + "&query=pasta" + "&number=2";
+        
         try {
-            return recipeRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Recipe not found"));
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to read the recipe file", e);
+            String response = restTemplate.getForObject(url, String.class);
+            System.out.println("API Response: " + response);
+          } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-}
+
+  }
