ca6914eda6e2e9707a6c3b62750d08e8ff643282
ikarrila <mjunposti@gmail.com>
2024-10-19 17:51:53 +0300

    Added filtering method

--- src/main/java/com/RecipeFinder/backend/controllers/PriceDataController.java
+++ src/main/java/com/RecipeFinder/backend/controllers/PriceDataController.java
@@ -2,12 +2,15 @@ package com.RecipeFinder.backend.controllers;
 
 import com.RecipeFinder.backend.models.PriceData;
 import com.RecipeFinder.backend.services.PriceDataService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/price")
@@ -16,11 +19,17 @@ public class PriceDataController {
     @Autowired
     private PriceDataService priceDataService;
 
-    @GetMapping("/all")
-    public List<PriceData> getAllFoodPriceData() {
+    @GetMapping
+    public PriceData getAllFoodPriceData() {
         return priceDataService.getAllFoodPriceData();
     }
 
-    // TODO: /filtered
+    @GetMapping("/filtered")
+    public PriceData getFilteredPriceData(
+            @RequestParam(value = "startMonth", required = false) String startMonth,
+            @RequestParam(value = "endMonth", required = false) String endMonth,
+            @RequestParam(value = "commodities", required = false) List<String> commodities) {
+        return priceDataService.getFilteredPriceData(startMonth, endMonth, commodities);
+    }
 
 }
