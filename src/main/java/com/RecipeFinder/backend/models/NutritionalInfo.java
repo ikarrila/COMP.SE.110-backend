9976cfea6bd36bf7c53858fd75c2e1e5c80d97d5
jadkarr <129285275+jadkarr@users.noreply.github.com>
2024-10-19 22:02:34 +0300

    applyIngredientFilter added
    Recipe related models updated and recipe service functions updated to take parameters

--- src/main/java/com/RecipeFinder/backend/models/NutritionalInfo.java
+++ src/main/java/com/RecipeFinder/backend/models/NutritionalInfo.java
@@ -3,9 +3,29 @@ package com.RecipeFinder.backend.models;
 public class NutritionalInfo {
     private double calories;
     private double protein;
-    private double fat;
-    private double carbohydrates;
+    private double carbs;
     
-    
-    // TODO: Getters and setters
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein (double protein) {
+        this.protein = protein;
+    }
+
+    public double getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(double carbs) {
+        this.carbs = carbs;
+    }
 }
--- src/main/java/com/RecipeFinder/backend/models/Recipe.java
+++ src/main/java/com/RecipeFinder/backend/models/Recipe.java
@@ -7,24 +7,18 @@ public class Recipe {
     private String name;
     private List<Ingredient> ingredients;
     private NutritionalInfo nutritionalInfo;
+    private List<String> diets;
+    private List<String> cuisines;
+
     private boolean isDairyFree;
     private boolean isGlutenFree;
     private boolean isSustainable;
+    private boolean isCheap;
+    private boolean isPopular;
+    private boolean isHealthy;
+
     private int healthScore;
 
-
-    // Default constructor
-    public Recipe() {}
-
-    // Constructor with fields
-    public Recipe(Long id, String name, List<Ingredient> ingredients, NutritionalInfo nutritionalInfo) {
-        this.id = id;
-        this.name = name;
-        this.ingredients = ingredients;
-        this.nutritionalInfo = nutritionalInfo;
-    }
-
-    // Getters and Setters example
     public Long getId() {
         return id;
     }
@@ -56,7 +50,77 @@ public class Recipe {
     public void setNutritionalInfo(NutritionalInfo nutritionalInfo) {
         this.nutritionalInfo = nutritionalInfo;
     }
+
+    public List<String> getDiets() {
+        return diets;
+    }
+
+    public void setDiets(List<String> diets) {
+        this.diets = diets;
+    }
+
+    public List<String> getCuisines() {
+        return cuisines;
+    }
+
+    public void setCuisines(List<String> cuisines) {
+        this.cuisines = cuisines;
+    }
+
+    public boolean getIsDairyFree() {
+        return isDairyFree;
+    }
+
+    public void setIsDairyFree(boolean isDairyFree) {
+        this.isDairyFree = isDairyFree;
+    }
+
+    public boolean getIsGlutenFree() {
+        return isGlutenFree;
+    }
+
+    public void setIsGlutenFree(boolean isGlutenFree) {
+        this.isGlutenFree = isGlutenFree;
+    }
+
+    public boolean getIsSustainable() {
+        return isSustainable;
+    }
+
+    public void setIsSustainable(boolean isSustainable) {
+        this.isSustainable = isSustainable;
+    }
+
+    public boolean getIsCheap() {
+        return isCheap;
+    }
+
+    public void setIsCheap(boolean isCheap) {
+        this.isCheap = isCheap;
+    }
+
+    public boolean getIsPopular() {
+        return isPopular;
+    }
+
+    public void setIsPopular(boolean isPopular) {
+        this.isPopular = isPopular;
+    }
+
+    public boolean getIsHealthy() {
+        return isHealthy;
+    }
+
+    public void setIsHealthy(boolean isHealthy) {
+        this.isHealthy = isHealthy;
+    }
+
+    public int getHealthScore() {
+        return healthScore;
+    }
+
+    public void setHealthScore (int healthScore) {
+        this.healthScore = healthScore;
+    }
+
 }
-
-
-//TODO: Add missing items from prototype, f.e. isCheap, isSustainable
