105a557d15d51987092540d8a58eda886ce15ee2
jadkarr <129285275+jadkarr@users.noreply.github.com>
2024-10-23 21:06:23 +0300

    SpoonacularApiRepository uodated
    applyIngredientFilter return list of Recipes and getRecipeInformation return the json

--- src/main/java/com/RecipeFinder/backend/repositories/SpoonacularAPIRepository.java
+++ src/main/java/com/RecipeFinder/backend/repositories/SpoonacularAPIRepository.java
@@ -2,6 +2,8 @@ package com.RecipeFinder.backend.repositories;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
+
+import com.RecipeFinder.backend.models.Ingredient;
 import com.RecipeFinder.backend.models.Recipe;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,61 +16,64 @@ import java.util.stream.Collectors;
 @Repository
 public class SpoonacularAPIRepository {
 
-
     private final String apiUrl = "https://api.spoonacular.com/recipes/complexSearch";
     private final String apiKey = "4b2cdc4ff73546b89cc40882c81c8e9c";
-        
-    private List<Integer> recipeIds = new ArrayList<>();
       
-    public void applyIngredientFilter(List<String> ingredients) {
+    public List<Recipe> applyIngredientFilter(List<String> ingredients) {
     RestTemplate restTemplate = new RestTemplate();
         //Creating comma separated string
     String includeIngrdients = ingredients.stream().collect(Collectors.joining(","));
     String url = apiUrl + "?apiKey=" + apiKey + "&query=pasta" + "&number=3" + "&includeIngredients=" + includeIngrdients;
     
     try {
-          String response = restTemplate.getForObject(url, String.class);
-          System.out.println("API Response: " + response);
+        String response = restTemplate.getForObject(url, String.class);
+        System.out.println("API Response: " + response);
+
+        //Parsing
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(response);
+        JsonNode results = root.path("results");
+
+        List<Recipe> recipes = new ArrayList<>();    
+
+        for (JsonNode result : results) {
+            Recipe recipe = new Recipe();
+            recipe.setId(result.path("id").asLong());
+            recipe.setName(result.path("title").asText());
+            recipes.add(recipe);
+            System.out.println("Recipe ID: " + recipe.getId() + "Name: " + recipe.getName());
+        }
+
+        return recipes;
+
+    } catch (Exception e) {
+            e.printStackTrace();
+    }
+    return new ArrayList<>();
+    }
     
-          //Parsing
-          ObjectMapper mapper = new ObjectMapper();
-          JsonNode root = mapper.readTree(response);
-          JsonNode results = root.path("results");
-    
-          recipeIds.clear();
-    
-          for (JsonNode result : results) {
-            int id = result.path("id").asInt();
-            recipeIds.add(id);
-            System.out.println("Recipe ID: " + id);
-          }
-    
-        } catch (Exception e) {
-                e.printStackTrace();
-            }
-      }
-    
-      private final String infoUrl = "https://api.spoonacular.com/recipes/{id}/information";
-      public void getRecipeInformation(Integer id) {
+    private final String infoUrl = "https://api.spoonacular.com/recipes/{id}/information";
+    public String getRecipeInformation(Integer id) {
         RestTemplate restTemplate = new RestTemplate();
         try {
-          String url = infoUrl.replace("{id}", id.toString()) + "?apiKey=" + apiKey;
-          String response = restTemplate.getForObject(url, String.class);
-          System.out.println("Recipe Information for ID " + id + ": " + response);
+            String url = infoUrl.replace("{id}", id.toString()) + "?apiKey=" + apiKey;
+            String response = restTemplate.getForObject(url, String.class);
+            System.out.println("Recipe Information for ID " + id + ": " + response);
+
+            return response;
         } catch (Exception e) {
-          e.printStackTrace();
-        }
-    
-      }
-    
-      public String extractItemName(String item) {
-        String[] parts = item.split(" ", 2);
-        //For error handling
-        if (parts.length > 1) {
-          return parts[1].toLowerCase();
-        }
-        return "";
+            e.printStackTrace();
     }
-   
+    return null;
+    }
+
+    public String extractItemName(String item) {
+    String[] parts = item.split(" ", 2);
+    //For error handling
+    if (parts.length > 1) {
+        return parts[1].toLowerCase();
+    }
+    return "";
+    }
 }
     
