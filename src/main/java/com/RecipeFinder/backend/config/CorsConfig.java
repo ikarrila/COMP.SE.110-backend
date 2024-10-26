eae0fb44819f2a1b4ceace63296e77d9a2c85067
BarryAlanPan <1504407704@qq.com>
2024-10-26 14:25:27 +0300

    Add recipes endpoints to corsFilter (direct update)

--- src/main/java/com/RecipeFinder/backend/config/CorsConfig.java
+++ src/main/java/com/RecipeFinder/backend/config/CorsConfig.java
@@ -18,6 +18,7 @@ public class CorsConfig {
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/api/**", config);
+        source.registerCorsConfiguration("/recipes/**", config);
         return new CorsFilter(source);
     }
 }
