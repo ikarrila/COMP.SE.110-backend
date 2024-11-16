cf1b0b1c4702afa98e434b63534b27ff4f8347ce
Emil <emil.selroos@gmail.com>
2024-11-16 13:21:21 +0200

    Add endpoint for updating users

--- src/main/java/com/RecipeFinder/backend/services/UserService.java
+++ src/main/java/com/RecipeFinder/backend/services/UserService.java
@@ -1,12 +1,14 @@
 package com.RecipeFinder.backend.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import com.RecipeFinder.backend.models.Recipe;
 import com.RecipeFinder.backend.models.User;
 import com.RecipeFinder.backend.repositories.UserRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,5 +34,26 @@ public class UserService {
             e.printStackTrace();
             return Optional.empty();
         }
+    }
+
+    public boolean updateUser(int id, String name, LocalDate birthday, String diet, String favouriteCuisine,
+            List<String> allergies, List<Recipe> savedRecipes) {
+        try {
+            Optional<User> userOptional = userRepository.getUserById(id);
+            if (userOptional.isPresent()) {
+                User user = userOptional.get();
+                user.setName(name);
+                user.setBirthday(birthday);
+                user.setDiet(diet);
+                user.setFavouriteCuisine(favouriteCuisine);
+                user.setAllergies(allergies);
+                user.setSavedRecipes(savedRecipes);
+                userRepository.saveUser(user);
+                return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
