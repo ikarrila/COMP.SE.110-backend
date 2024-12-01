8fac1991298dce243a870232167d997baff7db87
jadkarr <129285275+jadkarr@users.noreply.github.com>
2024-12-01 19:16:01 +0200

    Updated tests for recipecontroller

--- src/test/java/com/RecipeFinder/backend/controllers/RecipeControllerTest.java
+++ src/test/java/com/RecipeFinder/backend/controllers/RecipeControllerTest.java
@@ -10,6 +10,7 @@ import org.mockito.MockitoAnnotations;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 import java.util.List;
+import com.fasterxml.jackson.databind.JsonNode;
 
 class RecipeControllerTest {
 
@@ -24,18 +25,56 @@ class RecipeControllerTest {
         MockitoAnnotations.openMocks(this);
     }
 
-    /**@Test
-    void testApplyIngredientFilter() {
-        List<String> ingredients = List.of("tomato", "cheese");
+    @Test
+    void testGetIngredientRecipes() {
+        List<String> ingredients = List.of("tomato", "potato");
         Recipe mockRecipe = new Recipe();
         List<Recipe> mockRecipes = List.of(mockRecipe);
 
-        when(recipeService.applyIngredientFilter(ingredients)).thenReturn(mockRecipes);
+        when(recipeService.getIngredientRecipes(ingredients)).thenReturn(mockRecipes);
 
-        List<Recipe> response = recipeController.applyIngredientFilter(ingredients);
+        List<Recipe> response = recipeController.getIngredientRecipes(ingredients);
         assertEquals(1, response.size());
-        verify(recipeService, times(1)).applyIngredientFilter(ingredients);
-    }**/
+        verify(recipeService, times(1)).getIngredientRecipes(ingredients);
+    }
+
+    @Test
+    void testGetUserRecipes() {
+        List<String> ingredients = List.of("chicken", "garlic");
+        Recipe mockRecipe = new Recipe();
+        List<Recipe> mockRecipes = List.of(mockRecipe);
+
+        when(recipeService.getUserRecipes(ingredients)).thenReturn(mockRecipes);
+
+        List<Recipe> response = recipeController.getUserRecipes(ingredients);
+        assertEquals(1, response.size());
+        verify(recipeService, times(1)).getUserRecipes(ingredients);
+    }
+
+    @Test
+    void testGetMealplanRecipes() {
+        List<String> ingredients = List.of("onion", "pepper");
+        Recipe mockRecipe = new Recipe();
+        List<Recipe> mockRecipes = List.of(mockRecipe);
+
+        when(recipeService.getMealplanRecipes(ingredients)).thenReturn(mockRecipes);
+
+        List<Recipe> response = recipeController.getMealplanRecipes(ingredients);
+        assertEquals(1, response.size());
+        verify(recipeService, times(1)).getMealplanRecipes(ingredients);
+    }
+
+    @Test
+    void testGetActiveFilters() {
+        JsonNode mockFilters = mock(JsonNode.class);
+
+        when(recipeService.getActiveFilters()).thenReturn(mockFilters);
+
+        JsonNode response = recipeController.getActiveFilters();
+        assertEquals(mockFilters, response);
+        verify(recipeService, times(1)).getActiveFilters();
+    }
+
 
     @Test
     void testGetRecipeInformation() {
